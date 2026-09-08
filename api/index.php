e86a3793e42f486886e1a2e1e2baa801fe23f998
Najwan <najwanmuyassar16@gmail.com>
2026-09-08 13:46:40 +0700

    fix: set fallback broadcast driver to log

--- api/index.php
+++ api/index.php
@@ -7,6 +7,14 @@ $_ENV['VIEW_COMPILED_PATH'] = '/tmp/views';
 $_ENV['CACHE_STORE'] = 'array';
 $_ENV['SESSION_DRIVER'] = 'cookie';
 
+// Set default broadcast driver agar tidak error connection []
+if (empty($_ENV['BROADCAST_DRIVER'])) {
+    $_ENV['BROADCAST_DRIVER'] = 'log';
+}
+if (empty($_ENV['BROADCAST_CONNECTION'])) {
+    $_ENV['BROADCAST_CONNECTION'] = 'log';
+}
+
 // Buat folder temporary yang dibutuhkan jika belum ada
 if (!is_dir('/tmp/storage/logs')) {
     mkdir('/tmp/storage/logs', 0755, true);
